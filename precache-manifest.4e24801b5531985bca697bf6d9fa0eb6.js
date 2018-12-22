daff894820826551b31524f373800451c59fe6bc
Ouissem Hammami <hossi225@hotmail.fr>
2018-12-22 11:25:18 -0500

    Updates

---
+++ precache-manifest.4e24801b5531985bca697bf6d9fa0eb6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b31ef2db53ed3afb2e4",
+    "url": "/react_spreadsheet/static/js/runtime~main.6b31ef2d.js"
+  },
+  {
+    "revision": "4a578782bd3a57a175d5",
+    "url": "/react_spreadsheet/static/js/main.4a578782.chunk.js"
+  },
+  {
+    "revision": "faa56223b4fdcbef7fa2",
+    "url": "/react_spreadsheet/static/js/1.faa56223.chunk.js"
+  },
+  {
+    "revision": "4a578782bd3a57a175d5",
+    "url": "/react_spreadsheet/static/css/main.cb72267f.chunk.css"
+  },
+  {
+    "revision": "182464af6f18960c75847ccfc3d1cda4",
+    "url": "/react_spreadsheet/index.html"
+  }
+];
