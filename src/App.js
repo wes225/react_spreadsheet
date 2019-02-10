9778071bc7e297fbf9ff825d48f808a354ed708d
Ouissem Hammami <hossi225@hotmail.fr>
2019-02-10 16:17:54 -0500

    Adds testing config

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 
 import Sheet from './spreadsheet/Sheet'
 
-class App extends Component {
+export class App extends Component {
   render() {
     return (
     <Sheet/>
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {configure , shallow, mount} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Provider from 'react-redux'
+import store from './redux/store'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+configure({adapter:new Adapter()})
+
+describe('<App/>', ()=>{
+  const wrapper = shallow(  <App store={store}/>)
+ 
+  it('should render App', ()=>{
+console.log(wrapper.debug())
+  
+  })
+  it('should contain a Sheet element',()=>{
+    expect(wrapper.dive('Sheet').length).toEqual(1)
+  })
+})
